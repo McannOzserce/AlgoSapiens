de2a6559abaf0dbea2218ce7905d9880717a616a
McannOzserce <mert_ozserce@outlook.com>
2025-11-25 23:23:38 +0300

    change

--- algosapiens/main/data_collector.py
+++ algosapiens/main/data_collector.py
@@ -4,8 +4,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 from datacollection.data_collection import fetch_binance_data as mainfunction
-
-mainfunction(50000, 'BTCUSDT')
+from datacollection.data_read import load_data as read
+read()
+#mainfunction(50000, 'BTCUSDT')
 
 # 1. parameter determines how many candles will be drawn.
 # 2. parameter determines which coin's candles will be drawn.
